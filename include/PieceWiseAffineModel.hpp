bbf115e642a9952c6cf7c7436181ca38576f4f0c
Nimit Singhania <singhania.nimit@gmail.com>
2025-11-24 14:35:04 -0800

    Add definitions for piecewise affine models.

--- include/PieceWiseAffineModel.hpp
+++ include/PieceWiseAffineModel.hpp
@@ -30,7 +30,7 @@ struct affineFunction
  *     p(x) = f(x) <= 0, where f(x) is an affine function.
  * Note that this only represents strict inequality.
  */
-struct guardPredicate
+struct predicate
 {
     vector<float> coeff;
     bool evaluate(vector<float>& input)
@@ -43,4 +43,57 @@ struct guardPredicate
         result += coeff[input.size()];
         return result <  0.0;
     }
+};
+
+/* Guard Predicate: A Boolean predicate is a boolean combination of simple
+ * affine predicates. It consists of multiple clauses that are conjuncted
+ * together:
+ *   p(x) = c1(x) AND c2(x) AND ... AND cM(x).
+ * Each clause is a disjunction of simple affine predicates:
+ *  ci(x) = pi1(x) OR pi2(x) OR ... OR piN(x).
+ */
+struct guardPredicate
+{
+    struct orPredicate
+    {
+        vector<predicate> terms;
+        bool evaluate(vector<float>& input)
+        {
+            for (auto& t : terms)
+            {
+                if(t.evaluate(input))
+                    return true;
+            }
+            return false;
+       }
+    }
+
+    vector<orPredicate> clauses;
+    bool evaluate(vector<float>& input)
+    {
+        for (auto& c: clauses)
+        {
+            if (!c.evaluate(input))
+                return false;
+        }
+        return true;
+    }
+};
+
+/* Piecewise affine model: A piecewise affine model consists of regions, where
+ * each region is associated with an affine function and a guard predicate. The
+ * regions are also called modes which correspond to modes of operations which
+ * are switched to define multiple modes. Within each mode, the dynamics of the
+ * model are represented by an affine function while modes are separated by
+ * guards associated with each region. Only one of the guard predicates is true
+ * for any input point in the input space.
+ */
+struct piecewiseAffineModel
+{
+    struct region
+    {
+        affineFunction f;
+        guardPredicate g;
+    };
+    vector<region> regions;
 };
