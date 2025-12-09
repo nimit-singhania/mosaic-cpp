a36663d937933aa1cd4e75451a5387d8f7c09b59
Nimit Singhania <singhania.nimit@gmail.com>
2025-12-09 15:39:38 -0800

    Add RMSE for evaluation metric.

--- infer.cpp
+++ infer.cpp
@@ -14,9 +14,14 @@ int main(int argc, char** argv)
     auto model = loadModelJSON(argv[1]);
     auto test_data = loadData(argv[2]);
 
+    float squared_error = 0.0;
     for (auto & r : test_data)
     {
-        std::cout << "Expected: " << r.second << ", Inferred: " << model.evaluate(r.first) << std::endl;
+        float val = model.evaluate(r.first);
+        std::cout << "Expected: " << r.second << ", Inferred: " << val << std::endl;
+        squared_error += (r.second - val)*(r.second - val); 
     }
+    squared_error = squared_error/test_data.size();
+    std::cout << "RMSE: " << std::sqrt(squared_error) << std::endl;
     return 0;
 }
